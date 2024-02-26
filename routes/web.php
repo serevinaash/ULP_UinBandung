d8fc274adf6907f986ed82ebf445125c1325934a
Alfthrpy <alfthr378@gmail.com>
2024-02-26 19:00:34 +0700

    feat: logout and middleware

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Auth\GoogleLoginController;
+use App\Http\Controllers\LogoutController;
 use App\Http\Controllers\Auth\RegistrationController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Auth\LoginController;
@@ -22,27 +23,30 @@ use App\Http\Controllers\UjiKompetensiController;
 Route::get('auth/google', [GoogleLoginController::class, 'redirectToGoogle']);
 Route::get('auth/google/callback', [GoogleLoginController::class, 'handleCallback']);
 
-Route::get('/login', [LoginController::class, 'index']);
+Route::get('/login', [LoginController::class, 'index'])->name('login')->middleware('guest');
 Route::post("/login",[LoginController::class, "authenticate"]);
+Route::post('/logout',[LogoutController::class, 'logout'])->middleware('auth');
 
-Route::get('/register', [RegistrationController::class, 'index']);
+Route::get('/register', [RegistrationController::class, 'index'])->middleware('guest');
 Route::post('/submit-registration', [RegistrationController::class, 'register']);
 
 Route::get('/services/konseling', [KonselingController::class, 'index']);
 Route::get('/get-available-sessions', [KonselingController::class, 'getAvailableSessions']);
-Route::get('/register/konseling', [KonselingController::class, 'daftar']);
+Route::get('/register/konseling', [KonselingController::class, 'daftar'])->middleware('auth');
 Route::post('/submit-konseling', [KonselingController::class, 'create']);
 
 Route::get('/services/psikotes', [PsikotesController::class, 'index']);
-Route::get('/register/psikotes', [PsikotesController::class, 'daftar']);
+Route::get('/register/psikotes', [PsikotesController::class, 'daftar'])->middleware('auth');
 Route::get('/get-available-sessions', [PsikotesController::class, 'getAvailableSessions']);
-Route::get('/register/psikotes-kelompok', [PsikotesController::class, 'daftarKelompok']);
+Route::get('/register/psikotes-kelompok', [PsikotesController::class, 'daftarKelompok'])->middleware('auth');
 Route::post('/submit-psikotes', [PsikotesController::class, 'create']);
 Route::post('/submit-psikotes-kelompok', [PsikotesController::class, 'createKelompok']);
 
 Route::get('/services/uji-kompetensi', [UjiKompetensiController::class, 'index']);
-Route::get('/register/uji-kompetensi', [UjiKompetensiController::class, 'daftar']);
+Route::get('/register/uji-kompetensi', [UjiKompetensiController::class, 'daftar'])->middleware('auth');
 Route::post('/submit-ujikom', [UjiKompetensiController::class, 'create']);
+
+
 
 Route::get('/', function () {
     return view('home');
